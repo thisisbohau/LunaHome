91beca165ec9f4c83a866004a07ef0833fec1230
thisisbohau <d.bohaumilitzky@gmail.com>
2022-08-27 12:10:28 +0200

    Initial Commit

---
+++ LunaHome/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LunaHome
+//
+//  Created by David Bohaumilitzky on 27.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LunaHome/LunaHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaHomeApp.swift
+//  LunaHome
+//
+//  Created by David Bohaumilitzky on 27.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LunaHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
